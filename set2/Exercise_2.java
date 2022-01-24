991ace4ed97762e39bb21ce4eaf280f60a0eb799
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-01-24 18:05:44 +0000

    add Exercise 2

--- set2/Exercise_2.java
+++ set2/Exercise_2.java
@@ -1,2 +1,27 @@
-package set2;public class Exercise_2 {
+package set2;
+
+import java.util.Scanner;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        String input = "   b ri   ght net   work  ";
+        input = input.trim();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != ' ') {
+                if (input.charAt(i) == 'b') {
+                    output.append(Character.toUpperCase(input.charAt(i)));
+                    continue;
+                }
+                if (input.charAt(i) == 'n') {
+                    output.append(' ').append(Character.toUpperCase(input.charAt(i)));
+                    continue;
+                }
+                output.append(input.charAt(i));
+            }
+
+        }
+
+        System.out.println(output);
+    }
 }
