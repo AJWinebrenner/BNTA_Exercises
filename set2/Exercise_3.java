84f180ad52f4766e767976e3e2114d6e569d13ca
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-01-25 10:11:38 +0000

    add Exercise_3

---
+++ set2/Exercise_3.java
@@ -0,0 +1,18 @@
+package set2;
+
+public class Exercise_3 {
+    public static void main(String[] args) {
+        for (String arg: args) {
+            try {
+                int num = Integer.parseInt(arg);
+                if (num % 2 == 0) {
+                    System.out.println(num + " is even");
+                } else {
+                    System.out.println(num + " is odd");
+                }
+            } catch (Exception e) {
+                System.out.println("Must be an integer");
+            }
+        }
+    }
+}
